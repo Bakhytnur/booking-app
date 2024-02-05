9f79aa616e6bcc870771b7371d1f82c9e5501928
Bakhytnur <b.s.gabdullin@gmail.com>
2024-02-05 15:55:30 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import './App.css';
+import IndexPage from './pages/indexPage';
+import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
+import RegisterPage from './pages/RegisterPage';
+import axios from 'axios';
+import { UserContextProvider } from './UserContext';
+import ProfilePage from './pages/ProfilePage';
+import PlacesFormPage from './pages/PlacesFormPage';
+import PlacesPage from './pages/PlacesPage';
+
+axios.defaults.baseURL = 'http://127.0.0.1:4000';
+axios.defaults.withCredentials = true;
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<ProfilePage />} />
+          <Route path="/account/places" element={<PlacesPage />} />
+          <Route path="/account/places/new" element={<PlacesFormPage />} />
+          <Route path="/account/places/:id" element={<PlacesFormPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import react, { useState } from 'react'
+import { Link } from "react-router-dom";
+
+export default function RegisterPage() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [registrationStatus, setRegistrationStatus] = useState(null);
+
+    async function registerUser(e) {
+        e.preventDefault();
+        try {
+          await axios.post('/register', {
+            name,
+            email,
+            password,
+          });
+          //alert('Registration completed. Now you can log in!');
+          setRegistrationStatus('Registration completed. Now you can log in!');
+        } catch (e) {
+          //alert('Registration failed. Please try again later');
+          setRegistrationStatus('Registration failed. Please try again later');
+        }
+        
+    }
+
+    return (
+        <div className="mt-4 grow flex items-center justify-around">
+          <div className="mb-64">
+            <h1 className="text-4xl text-center mb-4">Register</h1>
+            <form className="max-w-md mx-auto" onSubmit={registerUser}>
+              <input type="text" placeholder='name'
+                value={name} onChange={e => setName(e.target.value)} />
+              <input type="email" placeholder='your@email.com' 
+                value={email} onChange={e => setEmail(e.target.value)} />
+              <input type="password" placeholder="password" 
+                value={password} onChange={e => setPassword(e.target.value)} />
+              <button className="primary">Register</button>
+              <div className="text-center py-2 text-gray-500">Already a member?
+                <Link className="underline text-black p-1" to={'/login'}>Login</Link>
+              </div>
+              {registrationStatus && <div>{registrationStatus}</div>}
+            </form>
+          </div>
+        </div>
+    )
+}

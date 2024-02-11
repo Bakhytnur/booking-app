efeaa60c4d012d83897db315df09085dc1f57857
Bakhytnur <b.s.gabdullin@gmail.com>
2024-02-12 00:04:52 +0600

    commit edit user booking page

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 const Place = require('./models/Place.js');
+const Booking = require('./models/Booking.js');
 const cookieParser = require('cookie-parser');
 const imageDownloader = require('image-downloader');
 const multer = require('multer');
@@ -34,6 +35,15 @@ app.use((req, res, next) => {
 app.use(morgan('combined')); 
 
 mongoose.connect(process.env.MONGO_URL);
+
+function getUserDataFromReq(req) {
+    return new Promise((resolve,reject) => {
+        jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+            if (err) throw err;
+            resolve(userData); 
+        });
+    });
+}
 
 // Routes
 app.get('/test', (req, res) => {
@@ -179,6 +189,26 @@ app.put('/places', async(req,res) => {
 app.get('/places', async(req,res) => {
     res.json( await Place.find());
 })
+
+app.post('/bookings', async (req,res) => {
+    const userData = await getUserDataFromReq(req);
+    const {
+        place,checkIn,checkOut,numberOfGuests,name,phone,price,
+    } = req.body;
+    Booking.create({
+        place,checkIn,checkOut,numberOfGuests,name,phone,price,
+        user:userData.id,
+    }).then((doc) => {
+        res.json(doc);
+    }).catch((err) => {
+        throw err;
+    })
+});
+
+app.get('/bookings', async (req,res) => {
+    const userData = await getUserDataFromReq(req);
+    res.json(await Booking.find({user:userData.id}).populate('place'));
+});
 
 app.listen(4000, () => {
     console.log("Server is running on port 4000");
